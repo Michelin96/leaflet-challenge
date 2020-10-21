faf53677cb46d3a446aeb6ac51b20eb520e24d77
MichelleH <voirdire96@gmail.com>
2020-10-20 19:17:27 -0700

    Adjusted map center and zoom

---
+++ static/js/logic.js
@@ -0,0 +1,25 @@
+// Create the map object
+let myMap = L.map("map", {
+    center: [14.23, -51.92],
+    zoom: 3
+  });
+
+// Make a title layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// Make the Quake marker layer
+
+// Make the legend
+
+// Make the tool tip 
+
+
+
+// Load the json data for the last 7 days
