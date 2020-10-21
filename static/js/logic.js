0b01fee0d8ab5eb04735ae56dd424a8a6ee630ae
MichelleH <voirdire96@gmail.com>
2020-10-20 19:43:29 -0700

    Added circle marker template

--- static/js/logic.js
+++ static/js/logic.js
@@ -14,13 +14,31 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
+// Make an array of quake data objects
+
+// Loop through the quake data array and create one marker for each location, bind a popup containing its name, magnitude and depth add it to the map
+for (let i = 0; i < cities.length; i++) {
+    let city = cities[i];
+    L.marker(city.location)
+      .bindPopup("<h1>" + city.name + "</h1> <hr> <h3>Population " + city.population + "</h3>")
+      .addTo(myMap);
+  }
+
+  // Make a circle marker for each quake
+  L.circle([45.52, -122.69], {
+    color: "green", // quake depth
+    fillColor: "green",
+    fillOpacity: 0.75,
+    radius: 500 // quake magnitude
+  }).addTo(myMap);
+
 // Make the Quake marker layer
 let quakeLocal = L.marker([45.52, -122.67], {
   }).addTo(myMap);
 
 // Make the legend
 
-// Make the tool tip/popup
+// Make the popup
 quakeLocal.bindPopup("Location: <br>Magnitude: <br>Depth:");
 
 
