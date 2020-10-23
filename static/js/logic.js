d492bc1a79859ee349d09bc119a15b34b2a2f77a
MichelleH <voirdire96@gmail.com>
2020-10-22 22:06:42 -0700

    Legend template setup

--- static/js/logic.js
+++ static/js/logic.js
@@ -44,4 +44,32 @@ function createFeatures(earthquakeData) {
     }
   });
 
+   // Set up the legend
+   var legend = L.control({ position: "bottomleft" });
+   legend.onAdd = function() {
+     var div = L.DomUtil.create("div", "info legend");
+     var limits = geojson.options.limits;
+     var colors = geojson.options.colors;
+     var labels = [];
+ 
+  // Add min & max
+  var legendInfo = `<h1>Median Income</h1>
+    <div class="labels">
+      <div class="min"> ${limits[0].toLocaleString(undefined,{style:'currency',currency:'USD',maximumSignificantDigits: 3})} </div>
+      <div class="max"> ${limits[limits.length - 1].toLocaleString(undefined,{style:'currency',currency:'USD',maximumSignificantDigits: 4})} </div>
+    </div>`;
+
+  div.innerHTML = legendInfo;
+
+  limits.forEach(function(limit, index) {
+    labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+  });
+
+  div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+  return div;
+};
+
+// Adding legend to the map
+legend.addTo(myMap);
+
 }
